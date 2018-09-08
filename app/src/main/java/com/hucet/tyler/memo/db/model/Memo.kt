f7ea4251c28c37c5b823b0a27a01d536fb589e2f
kimtaesu <kimtaesoo188@gmail.com>
2018-09-08 14:07:43 +0900

    add

--- app/src/main/java/com/hucet/tyler/memo/db/model/Memo.kt
+++ app/src/main/java/com/hucet/tyler/memo/db/model/Memo.kt
@@ -3,6 +3,7 @@ package com.hucet.tyler.memo.db.model
 import android.arch.persistence.room.*
 import android.os.Bundle
 import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
 import com.hucet.tyler.memo.ArgKeys
 import com.hucet.tyler.memo.db.converter.ArrayCheckItemConverter
 import com.hucet.tyler.memo.db.model.Memo.Companion.MEMO_TABLE
@@ -17,9 +18,10 @@ data class Memo(
         @Embedded
         val attr: MemoAttribute = MemoAttribute(false),
         val createAt: Long = System.currentTimeMillis(),
-//        @Embedded
-//        @TypeConverters(ArrayCheckItemConverter::class)
-//        var checkItems: List<CheckItem> = emptyList(),
+        @Embedded
+        @TypeConverters(ArrayCheckItemConverter::class)
+        @SerializedName("checkItems")
+        var checkItems: ArrayList<CheckItem>? = null,
         @Embedded
         var colorTheme: ColorTheme = ColorTheme.default.colorTheme,
         @PrimaryKey(autoGenerate = true)
--- app/src/main/java/com/hucet/tyler/memo/repository/memo/MemoViewDto.kt
+++ app/src/main/java/com/hucet/tyler/memo/repository/memo/MemoViewDto.kt
@@ -8,7 +8,6 @@ import com.hucet.tyler.memo.db.model.Memo
 internal data class MemoViewDto(
         @Embedded
         var memo: Memo = Memo.empty(),
-
         var labelIds: List<Long>? = null
 )
 
